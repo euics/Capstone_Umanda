b704aaeb5172480180f50e0b55ef1375cccab070
JongHun <pjh575157@gmail.com>
2023-03-26 23:25:50 +0900

    2023/03/26 회원정보를 JSON 형식으로 넘겨줄 ResponseUser 클래스 생성

---
+++ src/main/java/sejong/europlanner/vo/response/ResponseUser.java
@@ -0,0 +1,14 @@
+package sejong.europlanner.vo.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ResponseUser {
+    private Long id;
+
+    private String username;
+
+    private String name;
+}

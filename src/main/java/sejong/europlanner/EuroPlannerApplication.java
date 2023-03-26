78f318bdaf2f000170da1738122f6f256b3f3ae4
JongHun <pjh575157@gmail.com>
2023-03-27 00:02:23 +0900

    2023/03/26 BCryptPasswordEncoder @Bean으로 등록

--- src/main/java/sejong/europlanner/EuroPlannerApplication.java
+++ src/main/java/sejong/europlanner/EuroPlannerApplication.java
@@ -2,6 +2,8 @@ package sejong.europlanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class EuroPlannerApplication {
@@ -10,4 +12,8 @@ public class EuroPlannerApplication {
 		SpringApplication.run(EuroPlannerApplication.class, args);
 	}
 
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }

0b4b2b9eb9305ab042a043fef0f70f92f770c8e5
JongHun <pjh575157@gmail.com>
2023-03-27 00:12:13 +0900

    2023/03/27 PasswordEncoder를 통해 비밀번호 암호화 이후 DB에 저장

--- src/main/java/sejong/europlanner/service/UserServiceImpl.java
+++ src/main/java/sejong/europlanner/service/UserServiceImpl.java
@@ -2,6 +2,7 @@ package sejong.europlanner.service;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import sejong.europlanner.dto.UserDto;
@@ -11,24 +12,39 @@ import sejong.europlanner.vo.response.ResponseUser;
 
 @Service
 @Transactional
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
 
+    private final PasswordEncoder passwordEncoder;
+
     @Autowired
-    public UserServiceImpl(UserRepository userRepository) {
-            this.userRepository = userRepository;
-        }
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
-        @Override
-        public ResponseUser checkValidation(UserDto userDto) {
-            if(!userDto.getPassword().equals(userDto.getPassword1()))
-                throw new IllegalStateException("비밀번호가 일치하지 않습니다.");
+    public UserEntity createUser(UserDto userDto) {
+        UserEntity user = new UserEntity();
+        user.setId(userDto.getId());
+        user.setUsername(userDto.getUsername());
+        user.setPassword(passwordEncoder.encode(userDto.getPassword()));
+        user.setName(userDto.getName());
+        user.setBirthdate(userDto.getBirthdate());
+        user.setGender(userDto.getGender());
 
-            UserEntity savedUser = userRepository.findByUsername(userDto.getUsername());
-            if(savedUser != null)
-                throw new IllegalStateException("이미 존재하는 회원입니다.");
+        return user;
+    }
 
-            userRepository.save(new ModelMapper().map(userDto, UserEntity.class));
+    @Override
+    public ResponseUser checkValidation(UserDto userDto) {
+        if (!userDto.getPassword().equals(userDto.getPassword1()))
+            throw new IllegalStateException("비밀번호가 일치하지 않습니다.");
+
+        UserEntity savedUser = userRepository.findByUsername(userDto.getUsername());
+        if (savedUser != null)
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+
+        userRepository.save(createUser(userDto));
 
         return new ModelMapper().map(userDto, ResponseUser.class);
     }

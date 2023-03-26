469b67c8009081b41c9a9d671590b85a17ed1f37
JongHun <pjh575157@gmail.com>
2023-03-26 23:26:31 +0900

    2023/03/26 Service 계층에서 사용할 UserDto 클래스 생성

---
+++ src/main/java/sejong/europlanner/dto/UserDto.java
@@ -0,0 +1,19 @@
+package sejong.europlanner.dto;
+
+import lombok.Data;
+import sejong.europlanner.enumtype.Gender;
+
+@Data
+public class UserDto {
+    private String username;
+
+    private String password;
+
+    private String password1;
+
+    private String name;
+
+    private Integer birthdate;
+
+    private Gender gender;
+}

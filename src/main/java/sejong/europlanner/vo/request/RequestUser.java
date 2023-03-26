fb9eec294f1cb0ea3bf73aa9f557b5a9c7bc8d67
JongHun <pjh575157@gmail.com>
2023-03-26 23:22:46 +0900

    2023/03/26 회원가입하는 회원의 정보를 JSON 파일로 입력받을 RequestUser 클래스 생성

---
+++ src/main/java/sejong/europlanner/vo/request/RequestUser.java
@@ -0,0 +1,19 @@
+package sejong.europlanner.vo.request;
+
+import lombok.Data;
+import sejong.europlanner.enumtype.Gender;
+
+@Data
+public class RequestUser {
+    private String username;
+
+    private String password;
+
+    private String password1;
+
+    private String name;
+
+    private Integer birthdate;
+
+    private Gender gender;
+}

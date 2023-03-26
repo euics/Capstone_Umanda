60516c65dca4be4fedd278aed453c134f3d82949
JongHun <pjh575157@gmail.com>
2023-03-26 23:09:29 +0900

    2023/03/26 EuroPlanner Project initial commit

---
+++ src/main/java/sejong/europlanner/EuroPlannerApplication.java
@@ -0,0 +1,13 @@
+package sejong.europlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EuroPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EuroPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sejong/europlanner/EuroPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package sejong.europlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuroPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

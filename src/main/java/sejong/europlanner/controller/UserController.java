033c473040941d6423ae4d3d3a04dc1a1438ec8f
JongHun <pjh575157@gmail.com>
2023-03-26 23:26:10 +0900

    2023/03/26 UserController 작성

---
+++ src/main/java/sejong/europlanner/controller/UserController.java
@@ -0,0 +1,45 @@
+package sejong.europlanner.controller;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import sejong.europlanner.dto.UserDto;
+import sejong.europlanner.service.UserService;
+import sejong.europlanner.vo.request.RequestUser;
+import sejong.europlanner.vo.response.ResponseUser;
+
+import java.net.URI;
+
+@RestController
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/user/register")
+    public ResponseEntity<ResponseUser> createUser(@RequestBody RequestUser requestUser){
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        UserDto userDto = mapper.map(requestUser, UserDto.class);
+
+        ResponseUser responseUser = userService.checkValidation(userDto);
+
+        // 사용자 URI 생성
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentContextPath()
+                .path("/users/{id}")
+                .buildAndExpand(responseUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).body(responseUser);
+    }
+}

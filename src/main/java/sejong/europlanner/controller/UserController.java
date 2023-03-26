2a3927d33a94076ac7b1d0efa39f3a0b24ffe4ef
JongHun <pjh575157@gmail.com>
2023-03-27 00:07:09 +0900

    2023/03/27 URL 가독성을 위해 @RequestMapping 선언

--- src/main/java/sejong/europlanner/controller/UserController.java
+++ src/main/java/sejong/europlanner/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import sejong.europlanner.dto.UserDto;
@@ -16,6 +17,7 @@ import sejong.europlanner.vo.response.ResponseUser;
 import java.net.URI;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
     private final UserService userService;
 
@@ -24,7 +26,7 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PostMapping("/user/register")
+    @PostMapping("/register")
     public ResponseEntity<ResponseUser> createUser(@RequestBody RequestUser requestUser){
         ModelMapper mapper = new ModelMapper();
         mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);

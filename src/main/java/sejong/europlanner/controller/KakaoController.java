1869582046ca77da712ff0b78b650d1b264863da
JongHun <pjh575157@gmail.com>
2023-03-27 23:24:06 +0900

    2023/03/27 KakaoServiceImpl이 아닌 인터페이스를 선언하는 것으로 변경

--- src/main/java/sejong/europlanner/controller/KakaoController.java
+++ src/main/java/sejong/europlanner/controller/KakaoController.java
@@ -4,16 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import sejong.europlanner.service.serviceImpl.KakaoService;
+import sejong.europlanner.service.serviceImpl.KakaoServiceImpl;
 
 @RestController
 @RequestMapping("/kakao")
 public class KakaoController {
-    private final KakaoService oAuthService;
+    private final KakaoServiceImpl kakaoService;
 
     @Autowired
-    public KakaoController(KakaoService oAuthService) {
-        this.oAuthService = oAuthService;
+    public KakaoController(KakaoServiceImpl kakaoService) {
+        this.kakaoService = kakaoService;
     }
 
     /**
@@ -22,13 +22,13 @@ public class KakaoController {
      */
     @GetMapping("/login")
     public void kakaoCallback(@RequestParam String code) {
-        String accessToken = oAuthService.getKakaoAccessToken(code);
+        String accessToken = kakaoService.getKakaoAccessToken(code);
     }
 
     @PostMapping("/logout")
     public ResponseEntity<String> logout(@RequestHeader("Authorization") String accessToken) {
         try {
-            HttpStatus status = oAuthService.kakaoLogout(accessToken);
+            HttpStatus status = kakaoService.kakaoLogout(accessToken);
             return new ResponseEntity<>("Logout successful", status);
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);

27ea1ee87db1420ee4403693529b77ac9c53881c
JongHun <pjh575157@gmail.com>
2023-03-27 16:27:01 +0900

    2023/03/27 리팩토리 변경

--- src/main/java/sejong/europlanner/service/serviceinterface/UserService.java
+++ src/main/java/sejong/europlanner/service/serviceinterface/UserService.java
@@ -1,8 +1,11 @@
-package sejong.europlanner.service;
+package sejong.europlanner.service.serviceinterface;
 
 import sejong.europlanner.dto.UserDto;
+import sejong.europlanner.vo.response.ResponseLogin;
 import sejong.europlanner.vo.response.ResponseUser;
 
 public interface UserService {
     ResponseUser checkValidation(UserDto userDto);
+
+    ResponseLogin loginValidation(UserDto userDto);
 }

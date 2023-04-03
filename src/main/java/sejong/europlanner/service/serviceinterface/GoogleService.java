f8d98acfb6e2634d1f88f9a4a3aa812341ef4784
JongHun <pjh575157@gmail.com>
2023-04-03 20:39:23 +0900

    2023/04/03 구글 회원정보 및 토큰 반환값 메소드 선언

---
+++ src/main/java/sejong/europlanner/service/serviceinterface/GoogleService.java
@@ -0,0 +1,12 @@
+package sejong.europlanner.service.serviceinterface;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import sejong.europlanner.vo.response.ResponseGoogleUser;
+
+public interface GoogleService {
+    JsonNode getUserFromCode(String code) throws Exception;
+
+    ResponseGoogleUser toResponse(JsonNode userProfile);
+
+    ResponseGoogleUser setToken(ResponseGoogleUser responseGoogleUser);
+}

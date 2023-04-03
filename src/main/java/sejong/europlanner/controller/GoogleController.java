e7c606d63134fe29bb5a76c704ccd09028fe0215
JongHun <pjh575157@gmail.com>
2023-04-03 20:39:40 +0900

    2023/04/03 구글 로그인 Controller 생성

---
+++ src/main/java/sejong/europlanner/controller/GoogleController.java
@@ -0,0 +1,31 @@
+package sejong.europlanner.controller;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import sejong.europlanner.service.serviceinterface.GoogleService;
+import sejong.europlanner.vo.response.ResponseGoogleUser;
+import sejong.europlanner.vo.response.ResponseKakaoUser;
+
+@RestController
+@RequestMapping("/google")
+public class GoogleController {
+    private final GoogleService googleService;
+
+    @Autowired
+    public GoogleController(GoogleService googleService) {
+        this.googleService = googleService;
+    }
+
+    @GetMapping("/login")
+    public ResponseEntity<ResponseGoogleUser> googleCallback(@RequestParam String code) throws Exception{
+        JsonNode userProfile = googleService.getUserFromCode(code);
+        ResponseGoogleUser responseGoogleUser = googleService.setToken(googleService.toResponse(userProfile));
+
+        return ResponseEntity.ok().body(responseGoogleUser);
+    }
+}

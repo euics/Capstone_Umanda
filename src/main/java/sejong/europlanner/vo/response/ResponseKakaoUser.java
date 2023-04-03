5bd25c091e5d2d08dd00d69bbe0d66b10c05251b
JongHun <pjh575157@gmail.com>
2023-04-03 20:37:47 +0900

    2023/04/03 카카오 회원정보 반환값 클래스 생성

---
+++ src/main/java/sejong/europlanner/vo/response/ResponseKakaoUser.java
@@ -0,0 +1,14 @@
+package sejong.europlanner.vo.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ResponseKakaoUser {
+    private String name;
+
+    private String profile;
+
+    private String jwtToken;
+}

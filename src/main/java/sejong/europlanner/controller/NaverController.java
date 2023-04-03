cb427f96263465985f1e5b1bed39d33e0c3cee06
JongHun <pjh575157@gmail.com>
2023-04-03 17:58:12 +0900

    2023/04/03 네이버 로그인, 회원정보, 로그아웃 Controller 작성

---
+++ src/main/java/sejong/europlanner/controller/NaverController.java
@@ -0,0 +1,45 @@
+package sejong.europlanner.controller;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import sejong.europlanner.service.serviceinterface.NaverService;
+
+import java.io.IOException;
+import java.util.Collections;
+
+@RestController
+@RequestMapping("/naver")
+public class NaverController {
+
+    private final NaverService naverService;
+
+    @Autowired
+    public NaverController(NaverService naverService) {
+        this.naverService = naverService;
+    }
+
+
+    @PostMapping("/login")
+    public String naverCallback(@RequestParam("code") String code, @RequestParam("state") String state) {
+        return naverService.getAccessToken(code, state);
+    }
+
+    @PostMapping("/user")
+    public String naverGetUser(@RequestHeader("Authorization") String accessToken) throws IOException {
+        JsonNode userProfile = naverService.getUserProfile(accessToken);
+        return userProfile.toString();
+    }
+
+    @PostMapping("/logout")
+    public HttpStatus naverLogout(@RequestHeader("Authorization") String accessToken) {
+        ResponseEntity<String> response = naverService.logout(accessToken);
+
+        return response.getStatusCode();
+    }
+}

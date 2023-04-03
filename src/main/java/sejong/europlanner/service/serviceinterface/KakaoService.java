46d2789d8932cd09fe08f0a809811add73bea753
JongHun <pjh575157@gmail.com>
2023-04-03 20:39:14 +0900

    2023/04/03 카카오 회원정보 및 토큰 반환값 메소드 선언

--- src/main/java/sejong/europlanner/service/serviceinterface/KakaoService.java
+++ src/main/java/sejong/europlanner/service/serviceinterface/KakaoService.java
@@ -2,6 +2,8 @@ package sejong.europlanner.service.serviceinterface;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.http.ResponseEntity;
+import sejong.europlanner.vo.response.ResponseKakaoUser;
+import sejong.europlanner.vo.response.ResponseNaverUser;
 
 import java.io.IOException;
 
@@ -14,4 +16,8 @@ public interface KakaoService {
     ResponseEntity<String> logout(String accessToken);
 
     JsonNode getUserFromCode(String code) throws Exception;
+
+    ResponseKakaoUser toResponse(JsonNode userProfile);
+
+    ResponseKakaoUser setToken(ResponseKakaoUser responseKakaoUser);
 }

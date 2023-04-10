cf6dc1731b64b5d41172f8985ef164f3af00cddf
JongHun <pjh575157@gmail.com>
2023-04-10 16:14:35 +0900

    2023/04/10 디렉토리 변경

--- src/main/java/sejong/europlanner/exception/customexception/UserNotFoundException.java
+++ src/main/java/sejong/europlanner/exception/customexception/UserNotFoundException.java
@@ -1,4 +1,4 @@
-package sejong.europlanner.exception;
+package sejong.europlanner.exception.customexception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;

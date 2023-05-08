17a5fe7d671f4f158687e58eec2207b1089e8cff
JongHun <pjh575157@gmail.com>
2023-05-08 16:11:55 +0900

    2023/05/08 반환할 ResponseGetBoard 클래스 작성

---
+++ src/main/java/sejong/europlanner/vo/response/board/ResponseGetBoard.java
@@ -0,0 +1,18 @@
+package sejong.europlanner.vo.response.board;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ResponseGetBoard {
+    private Long id;
+    private String title;
+    private String content;
+    private String createdBy;
+    private String modifiedBy;
+    private LocalDateTime regTime;
+    private LocalDateTime updateTime;
+}

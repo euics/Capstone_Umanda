489c74ced489a5c845e0fc9a7ae1e6d58f2e25c0
JongHun <pjh575157@gmail.com>
2023-05-08 18:20:03 +0900

    2023/05/08 createComments 메소드 코드 구현

--- src/main/java/sejong/europlanner/service/serviceImpl/CommentsServiceImpl.java
+++ src/main/java/sejong/europlanner/service/serviceImpl/CommentsServiceImpl.java
@@ -8,9 +8,12 @@ import org.springframework.transaction.annotation.Transactional;
 import sejong.europlanner.dto.CommentsDto;
 import sejong.europlanner.entity.BoardEntity;
 import sejong.europlanner.entity.CommentsEntity;
+import sejong.europlanner.entity.UserEntity;
 import sejong.europlanner.exception.customexception.BoardNotFoundException;
 import sejong.europlanner.repository.BoardRepository;
+import sejong.europlanner.repository.CommentsRepository;
 import sejong.europlanner.service.serviceinterface.CommentsService;
+import sejong.europlanner.vo.request.comments.RequestCreateComments;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,10 +24,12 @@ import java.util.Optional;
 public class CommentsServiceImpl implements CommentsService {
 
     private final BoardRepository boardRepository;
+    private final CommentsRepository commentsRepository;
 
     @Autowired
-    public CommentsServiceImpl(BoardRepository boardRepository) {
+    public CommentsServiceImpl(BoardRepository boardRepository, CommentsRepository commentsRepository) {
         this.boardRepository = boardRepository;
+        this.commentsRepository = commentsRepository;
     }
 
     @Override
@@ -46,5 +51,24 @@ public class CommentsServiceImpl implements CommentsService {
         }
 
         return commentsDtoList;
+    }
+
+    @Override
+    public CommentsDto createComments(Long boardId, RequestCreateComments requestCreateComments) {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        CommentsDto mappedDto = mapper.map(requestCreateComments, CommentsDto.class);
+
+        Optional<BoardEntity> savedBoard = boardRepository.findById(boardId);
+        if(savedBoard.isEmpty())
+            throw new BoardNotFoundException("존재하지 않는 게시판입니다.");
+
+        UserEntity savedUser = savedBoard.get().getUser();
+
+        CommentsEntity commentsEntity = CommentsEntity.commentsEntity(mappedDto, savedBoard.get(), savedUser);
+        CommentsEntity savedComments = commentsRepository.save(commentsEntity);
+
+        return mapper.map(savedComments, CommentsDto.class);
     }
 }
--- src/main/java/sejong/europlanner/service/serviceinterface/CommentsService.java
+++ src/main/java/sejong/europlanner/service/serviceinterface/CommentsService.java
@@ -1,9 +1,12 @@
 package sejong.europlanner.service.serviceinterface;
 
 import sejong.europlanner.dto.CommentsDto;
+import sejong.europlanner.vo.request.comments.RequestCreateComments;
 
 import java.util.List;
 
 public interface CommentsService {
     List<CommentsDto> getCommentsList(Long boardId);
+
+    CommentsDto createComments(Long boardId, RequestCreateComments requestCreateComments);
 }

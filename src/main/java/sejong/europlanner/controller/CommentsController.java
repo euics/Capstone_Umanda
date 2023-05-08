4e1a32e46054d8ed19103e5656f22e2866a23739
JongHun <pjh575157@gmail.com>
2023-05-08 18:39:33 +0900

    2023/05/08 /comments/update/{commentsId} 엔드포인트 컨트롤러 계층 코드 구현

--- src/main/java/sejong/europlanner/controller/CommentsController.java
+++ src/main/java/sejong/europlanner/controller/CommentsController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import sejong.europlanner.dto.CommentsDto;
 import sejong.europlanner.service.serviceinterface.CommentsService;
 import sejong.europlanner.vo.request.comments.RequestCreateComments;
+import sejong.europlanner.vo.request.comments.RequestUpdateComments;
 import sejong.europlanner.vo.response.comments.ResponseGetComments;
 
 import java.util.ArrayList;
@@ -45,6 +46,20 @@ public class CommentsController {
     public ResponseEntity<ResponseGetComments> createComments(@PathVariable Long boardId,
                                                               @RequestBody RequestCreateComments requestCreateComments){
         CommentsDto commentsDto = commentsService.createComments(boardId, requestCreateComments);
+
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        ResponseGetComments responseGetComments = mapper.map(commentsDto, ResponseGetComments.class);
+        responseGetComments.setCommentsId(commentsDto.getId());
+
+        return ResponseEntity.ok().body(responseGetComments);
+    }
+
+    @PutMapping("/comments/update/{commentsId}")
+    public ResponseEntity<ResponseGetComments> updateComments(@PathVariable Long commentsId,
+                                                              @RequestBody RequestUpdateComments requestUpdateComments){
+        CommentsDto commentsDto = commentsService.updateComments(commentsId, requestUpdateComments);
 
         ModelMapper mapper = new ModelMapper();
         mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);

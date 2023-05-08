8596df3b2050db599ced5bf055a0defa112d0a04
JongHun <pjh575157@gmail.com>
2023-05-08 16:37:35 +0900

    2023/05/08 BoardEntity를 만드는 BoardEntity 매소드 코드 작성

--- src/main/java/sejong/europlanner/entity/BoardEntity.java
+++ src/main/java/sejong/europlanner/entity/BoardEntity.java
@@ -3,6 +3,7 @@ package sejong.europlanner.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import sejong.europlanner.dto.BoardDto;
 import sejong.europlanner.global.BaseEntity;
 
 import javax.persistence.*;
@@ -23,4 +24,14 @@ public class BoardEntity extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private UserEntity user;
+
+    public static BoardEntity boardEntity(BoardDto boardDto, UserEntity user) {
+        BoardEntity boardEntity = new BoardEntity();
+
+        boardEntity.setTitle(boardDto.getTitle());
+        boardEntity.setContent(boardDto.getContent());
+        boardEntity.setUser(user);
+
+        return boardEntity;
+    }
 }

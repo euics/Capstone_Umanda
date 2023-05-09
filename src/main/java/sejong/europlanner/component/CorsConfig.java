0ba04533a10835049d76f64a90311847eeef9398
JongHun <pjh575157@gmail.com>
2023-05-09 23:24:04 +0900

    2023/05/09 CORS 문제 해결을 위한 CorsConfig 코드 추가

---
+++ src/main/java/sejong/europlanner/component/CorsConfig.java
@@ -0,0 +1,18 @@
+package sejong.europlanner.component;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
